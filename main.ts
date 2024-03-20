61a26b03bfbc92bdf07b7a452f91deec1cd819d3
HinaAmin <160530350+hinaamin@users.noreply.github.com>
2024-03-20 22:05:29 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+
+//making months names array
+let monthsnames:string []= ["january" , "february" , "march" , "april" ];
+
+// print the messege of list
+console.log(monthsnames); 
+
+//print the value of the array in the from of list
+monthsnames.forEach(month => console.log(month));
+
+
